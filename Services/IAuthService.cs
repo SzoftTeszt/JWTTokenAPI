f198942f0fb8dd30a71e5395edfdd27178b459aa
Nagy János <janos@zold.lan>
2024-02-12 08:19:20 +0100

    Add project files.

---
+++ Services/IAuthService.cs
@@ -0,0 +1,20 @@
+﻿using JWTTokenAPI.Models;
+using System.Security.Claims;
+
+namespace JWTTokenAPI.Services
+{
+    public interface IAuthService
+    {
+        Task<(int, string)> Register(RegistrationModel model);
+        Task<(int, ApplicationUser?, string)> Login(LoginModel model);
+
+        Task<(int, List<ApplicationUser>)> UserList();
+       
+        Task<(int, IList<string>)> UserClaim(string id);
+
+        Task<(int, string)> SetClaims(RolesModel id);
+
+
+
+    }
+}
